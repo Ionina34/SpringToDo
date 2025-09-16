00fef3f4a9b10be596c3db6248c50fd33197aacd
Dasha <igoninadasha0315@mail.ru>
2025-09-16 21:25:45 +0500

    test

--- src/main/java/com/emobile/springtodo/SpringToDoApplication.java
+++ src/main/java/com/emobile/springtodo/SpringToDoApplication.java
@@ -8,7 +8,6 @@ public class SpringToDoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringToDoApplication.class, args);
-        System.out.println("test");
     }
 
 }
